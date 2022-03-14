72d6ce4af851fc38e4a1f76c1b28ae11b508b29b
Ben Fitzhardinge <ben@fitzhardinge.net>
2022-03-14 19:56:03 +0800

    updated requirements for heroku

--- project/settings.py
+++ project/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-by_+5&melil+c4
 DEBUG = True
 
 ALLOWED_HOSTS = ["https://sheltered-bayou-25697.herokuapp.com"]
+CORS_ALLOWED_ORIGINS = ["https://sheltered-bayou-25697.herokuapp.com"]
 
 # Application definition
 
@@ -122,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
